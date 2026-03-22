ade26bc53a6ae7068ad6561d0649915ac4e64e76
Rayya12 <rayaalului@gmail.com>
2026-03-22 07:29:18 +0700

    feat: creating user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('theme_preference')->default('light');
             $table->rememberToken();
             $table->timestamps();
         });
