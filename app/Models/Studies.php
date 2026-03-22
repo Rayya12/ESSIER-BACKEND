adf7aa81e0e79b365c4786efed6e96f5fb7b4399
Rayya12 <rayaalului@gmail.com>
2026-03-22 09:57:19 +0700

    feat: creating studies table

---
+++ app/Models/Studies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(["user_id",'title', 'status'])]
+class Studies extends Model
+{
+    use HasFactory;
+
+    # The table associated with the model.
+    protected $table = "studies";
+
+    # uuid
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2026_03_22_025141_create_studies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('studies', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw("gen_random_uuid()"));
+            $table->foreignUuid("user_id")
+                ->constrained()
+                ->onDelete('cascade');
+
+                $table->string("title");
+                $table->string("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('studies');
+    }
+};
