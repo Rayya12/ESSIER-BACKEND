4194691ee6b4680ba00e9a2d4ca2ca734c8c2f4a
Rayya12 <rayaalului@gmail.com>
2026-03-22 10:13:20 +0700

    feat: add questions table

---
+++ app/Models/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(["question_text","ideal_answer","ideal_embedding", "study_id","order_index"])]
+class Question extends Model
+{
+    use HasFactory;
+
+    # The table associated with the model.
+    protected $table = "questions";
+
+    #uuid
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function study(){
+        return $this->belongsTo(Study::class);
+    }
+}
