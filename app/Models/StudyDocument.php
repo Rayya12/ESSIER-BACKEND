c3bb7046ab95af54f6aebd02c76eeb167969478c
Rayya12 <rayaalului@gmail.com>
2026-03-22 10:03:54 +0700

    feat: adding study documents table

---
+++ app/Models/StudyDocument.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+
+#[Fillable(["study_id", "file_path", "file_name","extracted_text"])]
+class StudyDocument extends Model
+{
+    use HasFactory;
+
+    # The table associated with the model.
+    protected $table = "study_documents";
+
+    #uuid
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function study(){
+        return $this->belongsTo(Study::class);
+    }
+}
